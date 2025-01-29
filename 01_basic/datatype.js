ffbd6c731c66bc14f4616659ff7a0fb5e4c516d7
neha060705 <missneha6thjuly@gmail.com>
2025-01-29 16:34:02 +0000

    datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version 
+console.log( 3+3)
+//alert(3+3) // we are using nodejs , not browser 
+console.log(3+3);
+console.log("neha")
+
+let name="neha"
+let age=19
+let isLoggedIn=false
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=> standalone value
+//undefined=> value is not assigned
+//symbol =>unique
+//object
+
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+
+
