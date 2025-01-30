0c98efac3d08bf85b0ddbef31dc766497794516d
neha060705 <missneha6thjuly@gmail.com>
2025-01-30 13:29:14 +0000

    operation

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,17 @@
+let value =3
+let negvalue = -value
+//console.log(negvalue);
+
+let str1 ="hello"
+let str2 =" neha"
+let str3 = str1+str2
+console.log(str3);
+console.log("1"+ 2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true);
+console.log(+"");
+let num1 , num2,num3 
+num1 =num2= num3=2+2
+let gameCounter =100
+gameCounter++
