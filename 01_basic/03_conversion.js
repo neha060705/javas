69b6af3acd5ae0608d77dd1d3507c8c21d4e8735
neha060705 <missneha6thjuly@gmail.com>
2025-01-30 12:59:02 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,25 @@
+let score="33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNum=Number(score)
+//console.log(typeof valueInNum);
+//console.log(valueInNum);
+
+
+//let isLoggedIn="1"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//1=>true
+
+
+//let isLoggedIn="neha"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//"", false
+//"neha"=>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
