a98413dee6f32fb8bdf191a36032c38231390ff2
neha060705 <missneha6thjuly@gmail.com>
2025-02-01 18:13:52 +0000

    number and math

---
+++ 01_basic/08_number_math.js
@@ -0,0 +1,20 @@
+const score =400
+console.log(score)
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const otherNum =1234.54
+console.log(otherNum.toPrecision(4));
+const hundreds =1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+ //+++++++++++++++++++++++++MATHS++++++++++
+ console.log(Math)
+ console.log(Math.abs(-4));
+ console.log(Math.round(4.6))
+ console.log(Math.ceil(4.2))
+ console.log(Math.floor(4.9))
+ console.log(Math.min(3,5,6,7,2,1))
+ console.log(Math.max(6,3,6,65,75,43))
+console.log(Math.random()) // it gives the random value in between 0 and 1
