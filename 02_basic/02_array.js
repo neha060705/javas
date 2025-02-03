e395de37e2d3be05811c271c3e063dedc5e68103
neha060705 <missneha6thjuly@gmail.com>
2025-02-03 18:36:59 +0000

    array2

---
+++ 02_basic/02_array.js
@@ -0,0 +1,20 @@
+// const myarr=["hero","thor","spiderman"]
+// const dcarr=["flash","superman","Ironman"]
+// //myarr.push(dcarr)
+// console.log(myarr);
+// const allarr=myarr.concat(dcarr)
+// console.log(allarr);
+// const another_arr= [...myarr,...dcarr]
+// console.log(another_arr);
+
+
+//array formation
+console.log(Array.isArray("Neha"));
+console.log(Array.from("NehaYadav"));
+console.log(Array.from({name:"NehaYadav"})); // Interesting good question for interviews
+
+
+let score1=100
+let score2=200
+let score3= 300
+console.log(Array.of(score1,score2,score3));
