a4dd858bbf3bcd4acebd877d136447e84d8f5b23
neha060705 <missneha6thjuly@gmail.com>
2025-02-03 18:17:40 +0000

    array

---
+++ 02_basic/01_Array.js
@@ -0,0 +1,23 @@
+ const myarr =[2,3,4,5,6,7]
+// console.log(myarr);
+// console.log(myarr[1]);
+// myarr.push(8)
+// console.log(myarr);
+ myarr.push(9)
+// console.log(myarr);
+ myarr.pop()
+// console.log(myarr);
+// myarr.unshift(0)
+// console.log(myarr);
+// myarr.shift(1)
+// console.log(myarr);
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(7));
+
+
+//slice and splice
+console.log("A",myarr);
+console.log(myarr.slice(1,3));
+console.log("B",myarr);
+console.log(myarr.splice(1,3));
+console.log("C",myarr);
