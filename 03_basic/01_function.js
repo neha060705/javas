abb29a0297d6f7412121f630f021179fbb11e605
neha060705 <missneha6thjuly@gmail.com>
2025-02-07 18:58:18 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,18 @@
+function myname(){
+    console.log("N");
+    console.log("E");
+    console.log("H");
+    console.log("A");
+}
+myname()
+function addtwoNumber(number1,number2){
+    console.log(number1+number2);
+}
+addtwoNumber(3,null)
+
+function addTwoNum(number1,number2){
+    let result = number1 +number2
+    return result
+}
+const result =addTwoNum(3,5)
+console.log("result : ",result);
