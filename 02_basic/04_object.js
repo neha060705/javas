5b3b279af753be1a7dae19cbb850a79adc654837
neha060705 <missneha6thjuly@gmail.com>
2025-02-07 17:30:20 +0000

    objects

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -1,23 +1,38 @@
  //const tinderUser = new Object()
- const tinderUser ={}
- tinderUser.id="123abc"
- tinderUser.name="sammy"
- tinderUser.isLoggedIn =false
-// console.log(tinderUser);
-const regularUser ={
-    email:"some@gmail.com",
-    fullname:{
-        userfullname:{
-            firstname:"neha",
-            lastname:"yadav"
-        }
-    }
-}
-console.log(regularUser.fullname.userfullname.firstname);
+//  const tinderUser ={}
+//  tinderUser.id="123abc"
+//  tinderUser.name="sammy"
+//  tinderUser.isLoggedIn =false
+// // console.log(tinderUser);
+// const regularUser ={
+//     email:"some@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"neha",
+//             lastname:"yadav"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname.userfullname.firstname);
 
-const obj1 ={1:"a", 2:"b"}
-const obj2 = {3:"a", 4:"b"}
-const obj3 = Object.assign(obj1,obj2)
-const obj4=Object.assign({},obj1,obj2)
-console.log(obj3); 
-console.log(obj4);
+// const obj1 ={1:"a", 2:"b"}
+// const obj2 = {3:"a", 4:"b"}
+// const obj3 = Object.assign(obj1,obj2)
+// const obj4=Object.assign({},obj1,obj2)
+// console.log(obj3); 
+// console.log(obj4);
+
+
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor : "neha"
+}
+//course.courseInstructor
+const {courseInstructor}=course
+console.log(courseInstructor);
+
+const {courseInstructor:Instructor}=course
+console.log(Instructor);
+
+const navbar =(props.company)
