27f65a8cb5baba87bf1775ea89013c4573bf4be3
neha060705 <missneha6thjuly@gmail.com>
2025-02-10 18:09:47 +0000

    arrow

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,26 @@
+const user ={
+    username : "neha",
+    price:999,
+    welcomeMessage : function(){
+        console.log(`${this.username},welcome to website`);
+    console.log(this);//it used to change the context 
+    }
+    
+}
+// user.welcomeMessage ()
+// user.username= "sky"
+// user.welcomeMessage()
+
+console.log(this);
+
+function chai(){
+    let username="neha"
+    console.log(this);
+}
+//chai()
+
+// const addTwo =(num1+num2)=>{
+//     return num1+num2
+// }
+const addTwo =(num1,num2) => (num1+num2)
+console.log(addTwo(3,4))
